c4492bb212d21213ac72fb3c434baa7f1f739000
1805814 <Pulpul999>
2026-01-29 20:40:16 +0530

    modified footer page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,24 +1,53 @@
 import React from 'react'
-import { FaFacebook } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa"
+
 const Footer = () => {
   return (
-    <footer className='border-t'>
-        <div className='container mx-auto p-4 text-center flex flex-col lg:flex-row lg:justify-between gap-2'>
-            <p>All Rights Reserved 2025.</p>
-            <div className='flex items-center gap-4 justify-center text-2xl'>
-                <a href='' className='hover:text-primary-100'>
-                    <FaFacebook/>
-                </a>
-                <a href='' className='hover:text-primary-100'>
-                    <FaInstagram/>
-                </a>
-                <a href='' className='hover:text-primary-100'>
-                    <FaLinkedin/>
-                </a>
-            </div>
+    <footer className="border-t bg-white">
+      <div className="container mx-auto p-4 flex flex-col lg:flex-row lg:justify-between gap-6 text-center lg:text-left">
+        
+        {/* Left: Copyright */}
+        <p className="text-sm text-gray-600">
+          © 2025. All Rights Reserved.
+        </p>
+
+        {/* Middle: Contact Info */}
+        <div className="text-sm text-gray-700">
+          <p className="font-semibold mb-1">Contact</p>
+          <p>📍 Kumhaltole-05, Birgunj</p>
+          <p>📧 barnawalsuraj124@gmail.com</p>
+          <p>📞 +977-9809144777</p>
         </div>
+
+        {/* Right: Social Icons */}
+        <div className="flex items-center gap-4 justify-center text-2xl">
+          <a
+            href="https://www.facebook.com/profile.php?id=61587279509562"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-primary-100 transition"
+          >
+            <FaFacebook />
+          </a>
+
+          <a
+            href="https://www.instagram.com/kwiksupa/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-primary-100 transition"
+          >
+            <FaInstagram />
+          </a>
+
+          <a
+            href="#"
+            className="hover:text-primary-100 transition"
+          >
+            <FaLinkedin />
+          </a>
+        </div>
+
+      </div>
     </footer>
   )
 }
